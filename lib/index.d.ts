a923d2ceb7d1c24cc2ff2da16e167099db5bd494
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-08-28 11:03:46 +0900

    fix pathname

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 import * as FirebaseFirestore from '@google-cloud/firestore';
 export declare const initialize: (_firestore: FirebaseFirestore.Firestore) => void;
-export declare const path = "/event";
+export declare const path = "/eventtrigger";
 export interface EventTrigger {
     createdAt: Date;
     updatedAt: Date;

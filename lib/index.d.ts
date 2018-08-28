3a269890347c2514c8459e359e77abaab7bf7715
Kensuke Hoshikawa <kensuke-hoshikawa@cookpad.com>
2018-08-28 15:53:00 +0900

    add message

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,16 @@
 import * as FirebaseFirestore from '@google-cloud/firestore';
+/**
+ * You have to call `OnlyOnce.initialize(admin.firestore()) after admin.initializeApp().`
+ * @param _firestore admin.firestore()
+ */
 export declare const initialize: (_firestore: FirebaseFirestore.Firestore) => void;
 export declare const path = "/eventtrigger";
 export interface EventTrigger {
     createdAt: Date;
     updatedAt: Date;
 }
+/**
+ * return true if eventID was already triggred.
+ * @param eventID string
+ */
 export declare function wasTriggered(eventID: string): Promise<boolean>;
